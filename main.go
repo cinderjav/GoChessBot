b8f314e0749b1b75cda66bc12f1ed27634338520
Cinderjav <jnune006@fiu.edu>
2018-06-02 00:06:39 -0400

    hooked up api

--- main.go
+++ main.go
@@ -1,10 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
-	board, turn := fenParser("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+	mux := http.NewServeMux()
+	mux.Handle("/play", http.HandlerFunc(handlePlay))
+	log.Fatal(http.ListenAndServe("localhost:8000", mux))
+}
+
+func handlePlay(w http.ResponseWriter, req *http.Request) {
+	//"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+
+	var fenObject FenRequest
+	fmt.Println(req.Body)
+	err := json.NewDecoder(req.Body).Decode(&fenObject)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	fmt.Println(fenObject)
+	board, turn := fenParser(fenObject.Fen)
 	chessGame := getChessGame(board, turn)
 	executedMove := chessGame.executeMove()
-	fmt.Println(executedMove)
+	w.Write([]byte(executedMove))
+}
+
+type FenRequest struct {
+	Fen string
 }
